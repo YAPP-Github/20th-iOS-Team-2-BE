4e04a1c2cdd1a48063bca255d3068f4cc7c7aa66
mac_daehwan2yo <daehwan7989@naver.com>
2022-06-05 03:53:28 +0900

    fix (#2) : ErrorResponse 별도로 분리

--- core/src/main/java/com/yapp/core/error/GlobalExceptionHandler.java
+++ core/src/main/java/com/yapp/core/error/GlobalExceptionHandler.java
@@ -1,17 +1,12 @@
 package com.yapp.core.error;
 
-import static org.springframework.http.HttpStatus.*;
-
-import java.time.LocalDateTime;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.yapp.core.error.exception.BaseBusinessException;
+import com.yapp.core.error.response.ErrorResponse;
 
-import lombok.Builder;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -22,7 +17,7 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<ErrorResponse> handlerBaseBusinessException(BaseBusinessException e) {
 
 		return ResponseEntity.status(e.getStatus())
-							 .body(convertToErrorResponse(e.getStatus(), e.getDetail()));
+							 .body(new ErrorResponse(e));
 	}
 
 	@ExceptionHandler(Exception.class)
@@ -34,27 +29,6 @@ public class GlobalExceptionHandler {
 		e.printStackTrace();
 
 		return ResponseEntity.internalServerError()
-							 .body(convertToErrorResponse(INTERNAL_SERVER_ERROR.value(), e.toString()));
-	}
-
-	private ErrorResponse convertToErrorResponse(int status, String detail) {
-		return ErrorResponse.builder()
-							.status(status)
-							.detail(detail)
-							.build();
-	}
-
-	@Getter
-	private static class ErrorResponse {
-		private final LocalDateTime timestamp;
-		private final int status;
-		private final String detail;
-
-		@Builder
-		public ErrorResponse(int status, String detail) {
-			this.timestamp = LocalDateTime.now();
-			this.status = status;
-			this.detail = detail;
-		}
+							 .body(new ErrorResponse(e));
 	}
 }
---
+++ core/src/main/java/com/yapp/core/error/response/ErrorResponse.java
@@ -0,0 +1,28 @@
+package com.yapp.core.error.response;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+
+import com.yapp.core.error.exception.BaseBusinessException;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+	private final LocalDateTime timestamp;
+	private final int status;
+	private final String detail;
+
+	public ErrorResponse(BaseBusinessException e) {
+		this.timestamp = LocalDateTime.now();
+		this.status = e.getStatus();
+		this.detail = e.getDetail();
+	}
+
+	public ErrorResponse(Exception e) {
+		this.timestamp = LocalDateTime.now();
+		this.status = HttpStatus.INTERNAL_SERVER_ERROR.value();
+		this.detail = e.toString();
+	}
+}
