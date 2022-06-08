d5a3112a1816e88e8250062faf7a009ce663e670
mac_daehwan2yo <daehwan7989@naver.com>
2022-06-09 01:12:41 +0900

    add (#6) : API, ProfileMessage Persistence 구성

--- api/src/main/java/com/yapp/api/domain/user/service/UserService.java
+++ api/src/main/java/com/yapp/api/domain/user/service/UserService.java
@@ -3,6 +3,8 @@ package com.yapp.api.domain.user.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.yapp.api.domain.user.persistence.handler.ProfileMessageCommandHandler;
+import com.yapp.api.domain.user.persistence.handler.ProfileMessageQueryHandler;
 import com.yapp.api.domain.user.persistence.handler.UserCommandHandler;
 import com.yapp.api.domain.user.persistence.handler.UserQueryHandler;
 
@@ -14,5 +16,7 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 	private final UserCommandHandler userCommandHandler;
 	private final UserQueryHandler userQueryHandler;
+	private final ProfileMessageCommandHandler profileMessageCommandHandler;
+	private final ProfileMessageQueryHandler profileMessageQueryHandler;
 
 }
