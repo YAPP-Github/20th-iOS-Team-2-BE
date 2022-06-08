3e08f3ff1d64acf90623d03a5aee8256d991be95
mac_daehwan2yo <daehwan7989@naver.com>
2022-06-08 20:08:49 +0900

    init : add BaseEntity
    - createdAt (yy-MM-dd HH:mm, Asia/Seoul)

--- api/src/main/java/com/yapp/api/ApiApplication.java
+++ api/src/main/java/com/yapp/api/ApiApplication.java
@@ -1,10 +1,21 @@
 package com.yapp.api;
 
+import static java.util.TimeZone.*;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ApiApplication {
+
+	@PostConstruct
+	void started() {
+		setDefault(getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
