0394c34cd78ce0b2fa83d3d97735db1c32ec7431
mac_daehwan2yo <daehwan7989@naver.com>
2022-06-12 14:29:16 +0900

    chore (#6) : API,  JpaQueryFactory Bean 등록

---
+++ api/src/main/java/com/yapp/global/query/QueryConfig.java
@@ -0,0 +1,20 @@
+package com.yapp.global.query;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+@Configuration
+public class QueryConfig {
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Bean
+	public JPAQueryFactory jpaQueryFactory() {
+		return new JPAQueryFactory(entityManager);
+	}
+}
