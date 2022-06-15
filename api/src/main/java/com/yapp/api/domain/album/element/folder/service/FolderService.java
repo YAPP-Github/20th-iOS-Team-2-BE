14d62d8666faa5a17bf9caa7c3d31d505355f482
mac_daehwan2yo <daehwan7989@naver.com>
2022-06-15 16:06:45 +0900

    test (#21) : API, 사진 및 음성 업로드 api test 구성, failure

--- api/src/main/java/com/yapp/api/domain/album/element/folder/service/FolderService.java
+++ api/src/main/java/com/yapp/api/domain/album/element/folder/service/FolderService.java
@@ -1,11 +1,15 @@
 package com.yapp.api.domain.album.element.folder.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.yapp.api.domain.album.element.folder.persistence.handler.FolderCommandHandler;
 import com.yapp.api.domain.album.element.folder.persistence.handler.FolderQueryHandler;
 import com.yapp.api.domain.file.persistence.handler.FileCommandHandler;
+import com.yapp.api.domain.user.persistence.entity.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,4 +20,20 @@ public class FolderService {
 	private final FolderCommandHandler folderCommandHandler;
 	private final FolderQueryHandler folderQueryHandler;
 	private final FileCommandHandler fileCommandHandler;
+
+	@Transactional
+	public void uploadPhotos(User user, LocalDate date, List<String> photos) {
+		// 파일 업로드
+		// 폴더 업로그
+		// - 같은 날짜에 존재하지 않은 폴더  : 폴더 새롭게 생성
+		// - 같은 날짜에 존재하는 폴더 : 기존 폴더 그대로 사용
+	}
+
+	@Transactional
+	public void uploadRecordings(User user, LocalDate date, String title, String link) {
+		// 파일 업로드
+		// 폴더 업로그
+		// - 같은 날짜에 존재하지 않은 폴더  : 폴더 새롭게 생성
+		// - 같은 날짜에 존재하는 폴더 : 기존 폴더 그대로 사용
+	}
 }
