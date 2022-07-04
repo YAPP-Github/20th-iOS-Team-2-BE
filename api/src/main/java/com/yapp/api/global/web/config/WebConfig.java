11716423119e56c9fd0885821dfd9b082fff0b4d
mac_daehwan2yo <daehwan7989@naver.com>
2022-07-05 00:08:05 +0900

    chore : Cors Setting

--- api/src/main/java/com/yapp/api/global/web/config/WebConfig.java
+++ api/src/main/java/com/yapp/api/global/web/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.yapp.api.global.security.auth.resolver.MustAuthenticatedArgumentResolver;
@@ -20,5 +21,14 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(new MustAuthenticatedArgumentResolver());
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowCredentials(false)
+				.allowedOrigins("*")
+				.allowedMethods("*")
+				.allowedHeaders("*");
 	}
 }
