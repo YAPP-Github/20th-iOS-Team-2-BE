6e619c6141d9d1967e235a2f92a98578102e3835
mac_daehwan2yo <daehwan7989@naver.com>
2022-07-24 18:25:48 +0900

    query fix

--- api/src/main/java/com/yapp/api/domain/user/persistence/repository/UserRepository.java
+++ api/src/main/java/com/yapp/api/domain/user/persistence/repository/UserRepository.java
@@ -9,6 +9,6 @@ import com.yapp.api.domain.user.persistence.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	@Query(nativeQuery = true,
-		   value = "SELECT * FROM users u LEFT JOIN oauth_info ON u.id = o.user_id WHERE o.provider = :provider AND o.oauth_Id = :oauthId")
+		   value = "SELECT * FROM users u LEFT JOIN oauth_info o ON u.id = o.user_id WHERE o.provider = :provider AND o.oauth_Id = :oauthId")
 	Optional<User> findByProviderAndOauthId(String provider, String oauthId);
 }
